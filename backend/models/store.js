553cd9ef96bca9114a0f69260bddad4056baf4c2
unknown <alexandre8603@gmail.com>
2023-02-05 20:57:54 -0300

    create models

--- backend/models/store.js
+++ backend/models/store.js
@@ -1,2 +1,14 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const db = require('../db.js')
+
+const Store = db.define('store', {
+
+},
+{
+    modelName: 'Store',
+    tableName: 'stores'
+})
+
+module.exports = {
+    Store
+}
