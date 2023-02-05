8b2b12cbe2e6e8142aaef55056e9e7fc3b4b1541
unknown <alexandre8603@gmail.com>
2023-02-05 10:55:16 -0300

    initialize db

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const AdminJS = require('adminjs')
 const AdminJSExpress = require('@adminjs/express')
 const express = require('express')
-
+const db = require('./db.js');
 const PORT = 3000
 
 const start = async () => {
@@ -12,6 +12,7 @@ const start = async () => {
   const adminRouter = AdminJSExpress.buildRouter(admin)
   app.use(admin.options.rootPath, adminRouter)
 
+  db.sync(() => console.log("Banco de dados rodando..."))
   app.listen(PORT, () => {
     console.log(`AdminJS started on http://localhost:${PORT}${admin.options.rootPath}`)
   })
